f756b5aa12e4fea28984d1ac654c7455f942cca9
nandweza <allannandweza@gmail.com>
2023-11-13 20:22:37 -0700

    added user routes

--- backend/dist/controllers/user-controllers.js
+++ backend/dist/controllers/user-controllers.js
@@ -1,3 +1,12 @@
-export const getAllUsers = () => {
+import User from "../models/User.js";
+export const getAllUsers = async (req, res, next) => {
+    try {
+        const users = await User.find();
+        res.status(200).json({ message: "ok", users });
+    }
+    catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "ERROR", cause: error.message });
+    }
 };
 //# sourceMappingURL=user-controllers.js.map
--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,3 +1,30 @@
-export const getAllUsers = () => {
+import express from "express";
+import User from "../models/User.js";
 
+export const getAllUsers = async(
+    req: express.Request, 
+    res: express.Response, 
+    next: express.NextFunction
+) => {
+    try {
+        const users = await User.find();
+
+        res.status(200).json({ message: "ok", users });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "ERROR", cause: error.message })
+    }
+}
+
+export const userSignup = async(
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+) => {
+    try {
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "ERROR", cause: error.message })
+    }
 }
