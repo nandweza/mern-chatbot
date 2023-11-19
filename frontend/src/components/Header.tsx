a20bafc4c69ebeac9fa1d52f530878aa261563ac
nandweza <allannandweza@gmail.com>
2023-11-18 18:19:37 -0700

    added logo component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import  AppBar  from "@mui/material/AppBar";
 import  Toolbar  from "@mui/material/Toolbar";
+import Logo from "./shared/Logo";
 
 const Header = () => {
     return (
-        <AppBar sx={{ bgcolor: "black", position: "static"}}>
-            <Toolbar></Toolbar>
+        <AppBar 
+            sx={{ 
+                bgcolor: "transparent", 
+                position: "static", 
+                boxShadow: "none"
+            }}
+        >
+            <Toolbar sx={{ display: "flex" }}>
+                <Logo />
+            </Toolbar>
         </AppBar>
     );
 };
