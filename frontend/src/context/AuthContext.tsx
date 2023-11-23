afacc9bab04fb90705cf826ec4ae4de1fc4e1fcb
nandweza <allannandweza@gmail.com>
2023-11-22 18:59:43 -0700

    more updates

--- frontend/src/context/AuthContext.tsx
+++ frontend/src/context/AuthContext.tsx
@@ -1,4 +1,5 @@
 import { ReactNode, createContext, useContext, useEffect, useState } from "react";
+import { checkAuthStatus, loginUser } from "../helpers/api-communicator";
 
 type User = {
     name: string;
@@ -17,12 +18,26 @@ const AuthContext = createContext<UserAuth | null>(null);
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
     const [user, setUser] = useState<User | null>(null);
-    const [isLoggedIn, setIsLoggedIn] = useState(true);
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
 
     useEffect(() => {
         //fetch if the user's cookies are valid then skip login
+        async function checkStatus() {
+            const data = await checkAuthStatus();
+            if (data) {
+                setUser({ email: data.email, name: data.name });
+                setIsLoggedIn(true);
+            }
+        }
+        checkStatus();
     }, []);
-    const login = async (email: string, password: string) => {};
+    const login = async (email: string, password: string) => {
+        const data = await loginUser(email, password);
+        if (data) {
+            setUser({ email: data.email, name: data.name });
+            setIsLoggedIn(true);
+        }
+    };
     const signup = async (name: string, email: string, password: string) => {};
     const logout = async () => {};
 
@@ -36,4 +51,4 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
 
-export const UserAuth = () => useContext(AuthContext);
+export const useAuth = () => useContext(AuthContext);
