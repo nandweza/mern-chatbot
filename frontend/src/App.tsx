fdc1cf2fb36526afea14ed3992337e7cd8b328d4
nandweza <allannandweza@gmail.com>
2023-11-23 19:28:25 -0700

    more updates on chat routes and interface

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
-import Chats from "./pages/Chats";
+import Chat from "./pages/Chat";
 import NotFound from "./pages/NotFound";
 
 function App() {
@@ -15,7 +15,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/chat" element={<Chats />} />
+                <Route path="/chat" element={<Chat />} />
                 <Route path="*" element={<NotFound />} />
             </Routes>
         </main>
--- frontend/src/helpers/api-communicator.ts
+++ frontend/src/helpers/api-communicator.ts
@@ -10,9 +10,18 @@ export const loginUser = async ( email: string, password: string ) => {
 };
 
 export const checkAuthStatus = async () => {
-    const res = await axios.post("/user/auth-status");
+    const res = await axios.get("/user/auth-status");
     if (res.status !== 200) {
         throw new Error("Unable to authenticate");
+    }
+    const data = await res.data;
+    return data;
+};
+
+export const sendChatRequest = async (message: string) => {
+    const res = await axios.post("/chat/new", { message });
+    if (res.status !== 200) {
+        throw new Error("Unable to send chat");
     }
     const data = await res.data;
     return data;
